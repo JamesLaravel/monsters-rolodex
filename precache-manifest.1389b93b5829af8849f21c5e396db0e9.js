76fb00b53ff60ee96eef93f227527c0f1efb48a2
james anih <uchennaanih16@gmail.com>
2020-06-13 00:07:41 +0100

    Updates

---
+++ precache-manifest.1389b93b5829af8849f21c5e396db0e9.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "20e139b020ff03e39738354b21e5dfc8",
+    "url": "/monsters-rolodex/index.html"
+  },
+  {
+    "revision": "b1b80aea953d4552f732",
+    "url": "/monsters-rolodex/static/css/main.ebac855e.chunk.css"
+  },
+  {
+    "revision": "f9dcfd166a0dbd3a0d85",
+    "url": "/monsters-rolodex/static/js/2.e2e0115a.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/monsters-rolodex/static/js/2.e2e0115a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b1b80aea953d4552f732",
+    "url": "/monsters-rolodex/static/js/main.758ae1f8.chunk.js"
+  },
+  {
+    "revision": "7c7a2dec0da0b94e3a2d",
+    "url": "/monsters-rolodex/static/js/runtime-main.1dfbc620.js"
+  }
+]);
